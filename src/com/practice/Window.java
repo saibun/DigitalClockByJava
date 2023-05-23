2de8beacbf274c45483b7dcc1c668f054ccd2a6d
Saikat Chatterjee <csaikat66@gmail.com>
2023-05-23 12:07:24 +0530

    Timmer, ActionListener,datetime

--- src/com/practice/Window.java
+++ src/com/practice/Window.java
@@ -2,28 +2,55 @@ package com.practice;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.Font;  
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import javax.swing.Timer;  
 
 public class Window extends JFrame {
-    JLabel heading = new JLabel();    
-    JLabel Clocktext;
+    JLabel heading;    
+    JLabel clocktext;
     private Font font;   
     public Window(){
         String name= "Digital Clock";
         super.setTitle(name);
         super.setSize(500, 500);
         super.setLocation(700, 50);
-        this.Gui();
+        this.Gui();//Loading gui() method befor loading JFrame window
+        this.StartClock();
         super.setVisible(true);
         
         
     }
     public void Gui(){
+        heading=new JLabel("Saikat's Clock");//Give heading using JLable
         heading.setFont(getFont());
+        this.setLayout(new GridLayout(2,1));
+        this.add(heading);
+        clocktext=new JLabel("CLOCK--");
+        clocktext.setFont(getFont());
+        this.add(clocktext);
     }
     public Font getFont(){
-        font= new Font("", Font.BOLD, 20);
+        font= new Font("", Font.BOLD, 20);//making the font for text
         return font;
+    }
+    public void StartClock(){
+        //Note:-import javax.swing.Timer not java.util.Timer
+        Timer timmer = new Timer(1000,new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               //String datetime = new Date().toString();
+               String datetime = new Date().toLocaleString();
+               clocktext.setText(datetime);
+            }
+            
+        });
+        timmer.start();
+
     }
     
 }
